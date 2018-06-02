16e62caa421a5cd85b35c8b7dd04600d29dc3cd6
unknown <b4710202@gmail.com>
2018-06-02 10:20:03 +0700

    Initial...

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	require_once('./vendor/autoload.php');
+	require_once './vendor/autoload.php';
 	// Namespace
 	use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 	use \LINE\LINEBot;
@@ -10,7 +10,7 @@
 	$content = file_get_contents('php://input');
 	$events = json_decode($content, true);
 	$httpClient = new CurlHTTPClient($channel_token);
-					$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+	$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
 	if (!is_null($events['events'])) {
 		// Loop through each event
 		foreach ($events['events'] as $event) {
